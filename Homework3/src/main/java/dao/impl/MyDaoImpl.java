5bafa7ef2b5cb15b2fafba2018cecfb7ff82ae1c
shat79799 <shat79799@gmail.com>
2025-10-17 11:14:21 +0800

    initial commit

---
+++ Homework3/src/main/java/dao/impl/MyDaoImpl.java
@@ -0,0 +1,10 @@
+package dao.impl;
+
+public class MyDaoImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Homework3/src/main/java/service/impl/MyServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+public class MyServiceImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
